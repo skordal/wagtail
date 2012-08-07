a2048a9b26e3f3ce9882b2008065b880aaeee3b7
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2012-08-07 21:56:31 +0200

    Fixed write bug and added functionality for ANDing registers
    * The bug fixed caused offsets to be ignored or wrong when writing
      to registers.

--- kernel/io.h
+++ kernel/io.h
@@ -19,7 +19,7 @@ namespace wagtail
 		// at (base + offset):
 		template<typename T>
 		inline void write(T data, const void * base, unsigned int offset = 0)
-			{ *(((volatile T *) base) + offset) = data; }
+			{ *((volatile T *) ((unsigned int) base + offset)) = data; }
 
 		// Reads an element of the specified type T from the register located at
 		// the specified base + offset, does a bitwise OR of its value and the
@@ -27,6 +27,13 @@ namespace wagtail
 		template<typename T>
 		inline void or_register(T data, const void * base, unsigned int offset = 0)
 			{ T temp = read<T>(base, offset); temp |= data; write(temp, base, offset); }
+
+		// Reads an element of the specified type T from the register at the specified
+		// base + offset and does a bitwise AND of its value and the provided data value and
+		// writes it back to the register:
+		template<typename T>
+		inline void and_register(T data, const void * base, unsigned int offset = 0)
+			{ T temp = read<T>(base, offset); temp &= data; write(temp, base, offset); }
 	}
 }
 
