e8699376cfa070ee3a0f07c891f033e9bbcfeafd
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2012-11-23 22:59:51 +0100

    Removed the filesystem test code

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -26,55 +26,6 @@ extern "C" void kernel::kernel_main()
 	// Initialize the SD card controller:
 	sd::initialize();
 
-	// Test code for the filesystem interface:
-	partition * fat_partition = (partition *) device_mgr::get()->get_device_by_name("sd0p0");
-	if(fat_partition == nullptr)
-		kernel::message() << "sd0p0 is not a valid device/partition!" << kstream::newline;
-	else {
-		kernel::message() << "sd0p0 is a valid partition/device, going to test the filesystem interface on it..."
-			<< kstream::newline;
-		filesystem * fs = filesystem::initialize(fat_partition);
-		if(fs == nullptr)
-			kernel::message() << "Cannot initialize filesystem driver for sd0p0!" << kstream::newline;
-		else {
-			kernel::message() << "sd0p0 has volume label \"" << fs->get_label() << "\"" << kstream::newline;
-			kernel::message() << "sd0p0 free space: " << (int) fs->get_free_space() << kstream::newline;
-
-			direntry * testdir = fs->read_directory("/");
-			if(testdir == nullptr)
-				kernel::message() << "Cannot read directory /, directory does not exist"
-					<< kstream::newline;
-			else {
-				direntry * current = testdir;
-
-				// Print the directory listing:
-				kernel::message() << "Files in root directory of sd0p0:" << kstream::newline;
-				kernel::message() << "Type\tName\t\tSize" << kstream::newline;
-				do {
-					switch(current->get_type())
-					{
-						case direntry::type::directory:
-							kernel::message() << "Dir\t";
-							break;
-						case direntry::type::file:
-							kernel::message() << "File\t";
-							break;
-						default:
-							kernel::message() << "Other\t";
-							break;
-					}
-
-					kernel::message() << current->get_name() << "\t"
-						<< (int) current->get_size() << " b" << kstream::newline;
-
-					direntry * next = current->get_next();
-					delete current;
-					current = next;
-				} while(current != nullptr);
-			}
-		}
-	}
-
 	kernel::message() << "Kernel halting." << kstream::newline;
 }
 
