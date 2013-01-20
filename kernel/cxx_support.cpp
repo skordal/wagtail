736fc4378a8968b1744212049b63821d30f7fc9e
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2013-01-20 12:39:31 +0100

    Added memset and memcpy to support some compiler generated constructors

--- kernel/cxx_support.cpp
+++ kernel/cxx_support.cpp
@@ -15,6 +15,22 @@ extern "C" void __cxa_pure_virtual()
 	kernel::panic();
 }
 
+// Function required for copying memory in copy constructors:
+extern "C" void * memcpy(void * dest, void * src, unsigned int n)
+{
+	for(unsigned int i = 0; i < n; ++i)
+		*((char *) dest + i) = *((char *) src + i);
+	return dest;
+}
+
+// Functino required for setting memory in constructors:
+extern "C" void * memset(void * d, char c, unsigned int n)
+{
+	for(unsigned int i = 0; i < n; ++i)
+		*((char *) d + i) = c;
+	return d;
+}
+
 // Function required to use std::function objects without checking if they are nullptr:
 void std::__throw_bad_function_call()
 {
--- kernel/cxx_support.h
+++ kernel/cxx_support.h
@@ -10,5 +10,25 @@
 /** Function required to use pure virtual methods. */
 extern "C" void __cxa_pure_virtual();
 
+/**
+ * Memory copying function. This is sometimes emitted by the compiler when
+ * default constructors are constructed.
+ * @param dest destination.
+ * @param src source.
+ * @param n size of the area to copy.
+ * @return a pointer to the destination.
+ */
+extern "C" void * memcpy(void * dest, void * src, unsigned int n);
+
+/**
+ * Memory fill function. This is sometimes emitted by the compiler when
+ * default constructors are construced.
+ * @param s start of the area to fill.
+ * @param c the byte to fill the area with.
+ * @param n the size of the area to fill.
+ * @return a pointer to the destination area.
+ */
+extern "C" void * memset(void * d, char c, unsigned int n);
+
 #endif
 
